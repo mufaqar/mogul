c84d3db0a38c0fab5508b0b1656ec71f15609d98
Mufaqar <mufaqar@gmail.com>
2025-07-03 17:59:07 +0500

    tants

--- src/app/tenants/page.tsx
+++ src/app/tenants/page.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Container, Row, Col, Button, Card, Form, Image } from 'react-bootstrap';
-import Link from 'next/link';
-import p2 from '@/assets/images/properties/p-1.jpg'
-import MainHeader from '@/components/theme/MainHeader';
-import MainFooter from '@/components/theme/MainFooter';
+import { Container, Row, Col, Button, Card, Form, Image } from 'react-bootstrap'
+import MainHeader from '@/components/theme/MainHeader'
+import MainFooter from '@/components/theme/MainFooter'
 function page() {
   return (
     <>
-    <MainHeader/>
-    <Container><h2>Tenants</h2></Container>
-    
-    <MainFooter />
+      <MainHeader />
+      <Container>
+        <Row>
+          <h2>Tenants</h2>
+        </Row>
+      </Container>
+
+      <MainFooter />
     </>
   )
 }
