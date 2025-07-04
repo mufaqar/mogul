f98154d6a4a7daa125090afe316274ffc65dfd9a
Mufaqar <mufaqar@gmail.com>
2025-07-04 16:01:28 +0500

    updated

--- src/app/(admin)/agents/details/data.ts
+++ src/app/(admin)/agents/details/data.ts
@@ -1,5 +1,8 @@
-import avatar3 from '@/assets/images/users/avatar-3.jpg'
-import avatar4 from '@/assets/images/users/avatar-4.jpg'
+import avatar3 from '/assets/images/users/avatar-3.jpg'
+import avatar4 from '/assets/images/users/avatar-4.jpg'
+
+
+
 import { StaticImageData } from 'next/image'
 
 export type PropertyStatusType = {
